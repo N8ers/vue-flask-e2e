0efd7251722096f808568cc6588542b01c8bfba9
sheryak_n <nathanraysheryak@gmail.com>
2022-05-19 06:43:47 -0400

    basic vue setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vue-flask-e2e
+
+This is a POC for a full cypress e2e test.
+The goal is to figure out a methodology that works for the mono repo setup.
---
+++ vue-app/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const isLoading = ref(true);
+
+const getPunchLine = () => {
+  console.log("I will make a network request to the falsk api: '/punch-line'");
+};
+
+onMounted(() => {
+  console.log("I will make a network request to the flask api '/data'");
+});
+</script>
+
+<template>
+  <div>
+    <h1>Vue-Flask-e2e Test!!!</h1>
+    <div v-if="isLoading">Loading Data...</div>
+    <hr />
+    <div>
+      <h3>What did the chicken say to the farmer?</h3>
+      <button @click="getPunchLine">???</button>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
